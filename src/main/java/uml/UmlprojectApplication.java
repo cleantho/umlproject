3c5189d3f690e3c29a1285a96fd63378ee3cbdda
cleantho <cleantho@gmail.com>
2023-01-13 10:48:06 -0300

    Projeto criado

---
+++ src/main/java/uml/UmlprojectApplication.java
@@ -0,0 +1,13 @@
+package uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlprojectApplication.class, args);
+	}
+
+}
