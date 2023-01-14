b8a0cc77c92f905599c9283a6107445a87f6c775
cleantho <cleantho@gmail.com>
2023-01-14 18:39:51 -0300

    Estado e Cidade

---
+++ src/main/java/uml/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package uml.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import uml.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
