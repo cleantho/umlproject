4753718b50a8604c68f6ee65854df84c55e754b8
cleantho <cleantho@gmail.com>
2023-01-13 23:36:49 -0300

    Produto e associação muitos para muitos

--- src/main/java/uml/UmlprojectApplication.java
+++ src/main/java/uml/UmlprojectApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import uml.domain.Categoria;
+import uml.domain.Produto;
 import uml.repositories.CategoriaRepository;
+import uml.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class UmlprojectApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmlprojectApplication.class, args);
@@ -24,7 +29,20 @@ public class UmlprojectApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().add(cat1);
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
---
+++ src/main/java/uml/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package uml.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import uml.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
