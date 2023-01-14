2e5185ccc4d1fccf30beb92ee15f8d70b1ec5a14
cleantho <cleantho@gmail.com>
2023-01-13 22:30:27 -0300

    Criando operação de instanciação

--- src/main/java/uml/UmlprojectApplication.java
+++ src/main/java/uml/UmlprojectApplication.java
@@ -1,13 +1,30 @@
 package uml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import uml.domain.Categoria;
+import uml.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class UmlprojectApplication {
+public class UmlprojectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmlprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
